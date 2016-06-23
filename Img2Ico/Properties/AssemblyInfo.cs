e86c2b6fb87b5de262aa8b05c5c83dcbb3d64e04
Barnett Trzcinski <btrzcinski@gmail.com>
2016-06-22 22:49:10 -0700

    Set metadata

--- Img2Ico/Properties/AssemblyInfo.cs
+++ Img2Ico/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Img2Ico")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Converts 256x256 PNGs into a Windows icon file.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Barnett Trzcinski")]
 [assembly: AssemblyProduct("Img2Ico")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2016 Barnett Trzcinski.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
